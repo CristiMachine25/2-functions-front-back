b1eb31e38f6250b59329613b2a05f6ed11a25221
cristi <cristiangavrilean@gmail.com>
2023-06-06 13:48:38 +0300

    Adaugat Rest Api pentru Task ul 1(Task 3)

--- task1-mocha/server.js
+++ task1-mocha/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
- app.use(express.json());
+app.use(express.json())
 
  app.post('/api/removeNthFromEnd', (req, res) => {
    const { head, n } = req.body;
@@ -12,7 +12,6 @@ const PORT = 3000;
 
    res.json(result);
 });
-
 
 app.listen(
     PORT,
